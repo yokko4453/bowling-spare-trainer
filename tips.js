0e693bc51d201fc076de318be12b44b8d833f6f3
yokko4453 <satoh445@hotmail.co.jp>
2026-01-10 21:00:42 +0900

    Add files via upload

---
+++ tips.js
@@ -0,0 +1,44 @@
+	const tips = {
+	  "7": {
+		ball: "spare",
+		stance: "22",
+		target: "22",
+		note: ""
+	  },
+	  "10": {
+		ball: "spare",
+		stance: "27",
+		target: "15",
+		note: ""
+	  },
+	  "2-4-5": {
+		ball: "main",
+		stance: "ストライクスタンスから3枚右",
+		target: "ストライクコース",
+		note: ""
+	  },
+	  "2-4": {
+		ball: "main",
+		stance: "ストライクスタンスから3枚右",
+		target: "ストライクコース",
+		note: ""
+	  },
+	  "4-7-8": {
+		ball: "main",
+		stance: "ストライクスタンスから2枚右",
+		target: "ストライクコース",
+		note: ""
+	  },
+	  "4-7": {
+		ball: "spare",
+		stance: "25",
+		target: "22",
+		note: ""
+	  },
+	  "6-10": {
+		ball: "spare",
+		stance: "25",
+		target: "15",
+		note: ""
+	  }
+	};
